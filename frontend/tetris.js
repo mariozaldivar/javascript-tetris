746afc90cee3b2de24a0f5680025c01c908f9727
mariozaldivar <mario.zaldivardlc@gmail.com>
2026-05-02 17:43:14 -0600

    Se añadió funcionalidad de Score, se muestra la Hold Piece, se corrigió el bug de la tecla espacio y el botón. Falt acorregir el volver a presioanr start-game

--- frontend/tetris.js
+++ frontend/tetris.js
@@ -2,18 +2,27 @@ const BOARD_WIDTH = 10;
 const BOARD_HEIGHT = 20;
 const board = Array.from({ length: BOARD_HEIGHT }, () => Array(BOARD_WIDTH).fill(0));
 let boardElement;
-let startButton;
-
+let startButtonElement;
+let scoreElement;
+let levelElement;
+let holdPieceElement;
+let gameOverElement;
 
 let currentRow;
 let currentCol;
 let currentShape;
 
-let lineClearCount;
-let score;
+let lineClearCount = 0;
+let lastClearedLinesCounter;
+let score = 0;
 let multiplyer;
+let level;
 
 let holdShape;
+let holdedThisTurn;
+
+
+let playing;
 
 
 const allShapes = [
@@ -56,7 +65,6 @@ const allShapes = [
     [7, 7, 7],
     [0, 0, 0]
   ],
-
 ];
 
 function drawBoard() {
@@ -115,22 +123,69 @@ function checkIfRowFull(row) {
   return true;
 }
 
-function makeLineClears() {
-  // TODO: Tetrises don't get done for some reason
-  for (let i = BOARD_HEIGHT - 1; i > 0; i--) {
-    if (checkIfRowFull(i)) {
-      for (let movingRow = i; movingRow > 0; movingRow--) {
-        for (let j = 0; j < BOARD_WIDTH; j++) {
-          board[movingRow][j] = board[movingRow - 1][j];
-        }
-      }
-      for (let j = 0; j < BOARD_WIDTH; j++) {
-        board[0][j] == 0;
-      }
-      i = BOARD_HEIGHT
+function clearLine(line) {
+  for (let movingRow = line; movingRow > 0; movingRow--) {
+    for (let j = 0; j < BOARD_WIDTH; j++) {
+      board[movingRow][j] = board[movingRow - 1][j];
     }
   }
+  for (let j = 0; j < BOARD_WIDTH; j++) {
+    board[0][j] == 0;
+  }
+
 }
+
+function makeLineClears() {
+  let clears = 0;
+  for (let i = BOARD_HEIGHT - 1; i > 0; i--) {
+    if (checkIfRowFull(i)) {
+      clearLine(i);
+      i = BOARD_HEIGHT;
+      clears++;
+    }
+  }
+
+  lastClearedLinesCounter = clears;
+  lineClearCount += clears;
+  updateScore(clears);
+
+}
+
+function updateScore(lines) {
+  let points;
+  switch (lines) {
+    case 0:
+      points = 0;
+      break;
+    case 1:
+      points = 100;
+      break;
+    case 2:
+      points = 200;
+      break;
+    case 3:
+      points = 300;
+      break;
+    case 4:
+      points = 400;
+      break;
+    default:
+      console.log("There's been a problem with lines");
+      break;
+  }
+  console.log("Lines: " + lines);
+  level = Math.floor((lineClearCount / 10) + 1);
+  console.log("Level: " + level)
+  console.log("Points:" + points);
+  score += (points * level)
+  console.log(score);
+
+  scoreElement.textContent = "Score: " + score;
+
+  levelElement.textContent = "Level: " + level;
+
+}
+
 
 function generateNewPiece() {
   makeLineClears()
@@ -149,6 +204,7 @@ function generateNewPiece() {
 
   }
 
+  holdedThisTurn = false;
   drawBoard();
 
 }
@@ -232,6 +288,7 @@ function drawCurrentPiece(row, col) {
 function pieceLower() {
   if (canBeDrawn(currentRow + 1, currentCol, currentShape)) {
     drawCurrentPiece(currentRow + 1, currentCol, currentShape);
+
   } else {
     drawCurrentPiece(currentRow, currentCol, currentShape);
 
@@ -241,8 +298,12 @@ function pieceLower() {
 function hardDrop() {
   while (canBeDrawn(currentRow + 1, currentCol, currentShape)) {
     drawCurrentPiece(currentRow + 1, currentCol, currentShape)
+
+    score += 2;
   }
   drawCurrentPiece(currentRow, currentCol, currentShape);
+
+  scoreElement.textContent = "Score: " + score;
   generateNewPiece();
 
 }
@@ -265,43 +326,101 @@ function attemptRotation() {
   }
 }
 
+function drawHoldPiece(shape) {
+
+  holdPieceElement.replaceChildren();
+  let currentBlock;
+  let currentRow;
+  for (let i = 0; i < shape.length; i++) {
+    currentRow = document.createElement("div");
+    currentRow.className = "row";
+    for (let j = 0; j < shape.length; j++) {
+      currentBlock = document.createElement("div");
+      currentBlock.className = "block";
+      switch (shape[i][j]) {
+        case 0:
+          currentBlock.style.opacity = 0;
+          break;
+        case 1:
+          currentBlock.style.backgroundColor = "red";
+          break;
+        case 2:
+          currentBlock.style.backgroundColor = "blue";
+          break;
+        case 3:
+          currentBlock.style.backgroundColor = "yellow";
+          break;
+        case 4:
+          currentBlock.style.backgroundColor = "green";
+          break;
+        case 5:
+          currentBlock.style.backgroundColor = "orange";
+          break;
+        case 6:
+          currentBlock.style.backgroundColor = "pink";
+          break;
+        case 7:
+          currentBlock.style.backgroundColor = "brown";
+          break;
+        default:
+          currentBlock.style.backgroundColor = "black";
+          break;
+      }
+
+
+      currentRow.appendChild(currentBlock);
+    }
+    holdPieceElement.appendChild(currentRow);
+  }
+}
 
 function holdPiece() {
-  undrawCurrentPiece();
-  let buffer;
-  buffer = holdShape;
-  holdShape = currentShape;
-  if (buffer != undefined) {
-    currentShape = buffer;
-  } else {
-    currentShape = allShapes[Math.floor(Math.random() * allShapes.length)];
+  if (!holdedThisTurn) {
+    undrawCurrentPiece();
+    let buffer;
+    buffer = holdShape;
+    holdShape = currentShape;
+    if (buffer != undefined) {
+      currentShape = buffer;
+
+    } else {
+      currentShape = allShapes[Math.floor(Math.random() * allShapes.length)];
+    }
+
+    drawHoldPiece(holdShape)
+
+
+    currentRow = 0;
+    currentCol = 4;
+    drawCurrentPiece(currentRow, currentCol, currentShape);
+    holdedThisTurn = true;
   }
-  currentRow = 0;
-  currentCol = 4;
-  drawCurrentPiece(currentRow, currentCol, currentShape);
+
 
 }
 
 
 const handleInput = (event) => {
-  console.log(event.key);
-  if (event.key == "ArrowRight") {
-    movePiece(currentRow, currentCol + 1);
-  }
-  else if (event.key == "ArrowLeft") {
-    movePiece(currentRow, currentCol - 1);
-  }
-  else if (event.key == "ArrowDown") {
-    movePiece(currentRow + 1, currentCol);
-  }
-  else if (event.key == "ArrowUp") {
-    attemptRotation();
-  }
-  else if (event.key == "c") {
-    holdPiece();
-  }
-  else if (event.key == " ") {
-    hardDrop();
+  if (playing) {
+    console.log(event.key);
+    if (event.key == "ArrowRight") {
+      movePiece(currentRow, currentCol + 1);
+    }
+    else if (event.key == "ArrowLeft") {
+      movePiece(currentRow, currentCol - 1);
+    }
+    else if (event.key == "ArrowDown") {
+      movePiece(currentRow + 1, currentCol);
+    }
+    else if (event.key == "ArrowUp") {
+      attemptRotation();
+    }
+    else if (event.key == "c") {
+      holdPiece();
+    }
+    else if (event.key == " ") {
+      hardDrop();
+    }
   }
 }
 
@@ -312,6 +431,7 @@ function startGame() {
       board[i][j] = 0;
     }
   }
+  playing = true;
   console.log("Se ha llamado startGame")
 
   generateNewPiece();
@@ -327,14 +447,20 @@ function startGame() {
   const lowering = setInterval(() => { pieceLower(); }, 500)
   globalThis.lowering = lowering;
 
+  score = 0;
+  level = 1;
 }
 
 function gameOver() {
   if (globalThis.lowering != undefined) {
     clearInterval(globalThis.lowering);
   }
-  let gameOverElement = document.getElementById("game-over");
-  document.removeEventListener("keydown", handleInput)
+  let gameOverText = document.createElement("h2");
+  gameOverText.textContent = "¡¡Game Over!!";
+  gameOverElement.appendChild(gameOverText);
+  playing = false;
+  document.removeEventListener("keydown", handleInput);
+
 
 
 }
@@ -343,10 +469,15 @@ function gameOver() {
 
 document.addEventListener("DOMContentLoaded", () => {
   boardElement = document.getElementById("board");
-  startButton = document.getElementsByName("start-button");
-
+  startButtonElement = document.getElementById("start-button");
+  scoreElement = document.getElementById("score")
+  levelElement = document.getElementById("level")
+  holdPieceElement = document.getElementById("hold-piece")
+  gameOverElement = document.getElementById("game-over")
   document.addEventListener("keydown", handleInput)
   drawBoard();
+
+
 })
 
 

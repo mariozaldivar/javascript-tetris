36f5305e875caf75a15de332ffea85c4a8bbf3aa
mariozaldivar <mario.zaldivardlc@gmail.com>
2026-05-23 00:39:56 -0600

    Added first routes of the API

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
 const express = require("express")
+const pool = require("./db")
+
+
 const app = express()
+const port = 3000
 
-
-app.listen(3000)
+app.listen(port, () => { console.log("Server has started") });
+app.use(express.json());
 
 app.get('/', (req, res) => {
 
-  res.status(500).json({ message: "Error" })
+  res.status(200).json({ message: "El servidor está conectado, bienvenido a la Tetris API" })
+
+})
+
+app.post("/getScores", async (req, res) => {
+
+  try {
+    const result = await pool.query("SELECT * FROM scores;");
+    res.json(result.rows);
+  }
+  catch (err) {
+    console.log(`Error: ${err}`);
+    res.sendStatus(500);
+  }
+
 
 
 })
+
+
